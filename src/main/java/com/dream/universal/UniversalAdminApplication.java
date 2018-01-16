e959bdb214755c35a8e5650d76f75931dee4b23b
nikolabai <nikola.vip@foxmail.com>
2018-01-16 20:47:45 +0800

    20180116-blank

---
+++ src/main/java/com/dream/universal/UniversalAdminApplication.java
@@ -0,0 +1,12 @@
+package com.dream.universal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalAdminApplication.class, args);
+	}
+}
